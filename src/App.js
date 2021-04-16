9f8694b765100e5e954fbbb7d5980ffadb47bc0e
yaltha <yaltharullist@gmail.com>
2021-04-16 19:18:49 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CountDown from "./components/CountDown";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CountDown
+        timeTillDate="05 13 2020, 6:00 am"
+        timeFormat="MM DD YYYY, h:mm a"
+      />
     </div>
   );
 }
